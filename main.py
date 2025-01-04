8f4ed6a3f0ce8d4bc937b8b011e159a35ebe135c
jobi joseph <jobijoseph16@gmail.com>
2025-01-04 10:10:57 +0530

    limited the news, add url, add language argument, add subject

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ import requests
 from send_email import send_email
 
 api_key = "bc07434d1e9a420cb0983d8f5ac3b60a"
+topic = "tesla"
 
-url = "https://newsapi.org/v2/everything?q=tesla&from=2024-12-04&sortBy=publishedAt&apiKey=bc07434d1e9a420cb0983d8f5ac3b60a"
+url = ("https://newsapi.org/v2/everything?"
+       f"q={topic}&from=2024-12-04&"
+       "sortBy=publishedAt&"
+       "apiKey=bc07434d1e9a420cb0983d8f5ac3b60a&""language=en")
 
 request = requests.get(url)
 content = request.json()
@@ -11,8 +15,11 @@ print(content)
 
 body = ""
 
-for article in content["articles"]:
-   body = body + str(article["title"]) + "\n" + str(article["description"]) + 2*"\n"
+for article in content["articles"][0:20]:
+   body = ("Subject: Today's news" +"\n" +
+           body + str(article["title"]) + "\n" +
+           str(article["description"]) + "\n" +
+           article["url"]+ 2*"\n")
 
 body = body.encode("utf-8")
 send_email(message = body)
